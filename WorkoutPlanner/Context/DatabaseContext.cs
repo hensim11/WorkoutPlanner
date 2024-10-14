dedc127f14628290c2600cf1ba70b182051b59a5
hensim11 <164030740+hensim11@users.noreply.github.com>
2024-10-14 10:34:13 +0100

    halfway through database seeder

--- WorkoutPlanner/Context/DatabaseContext.cs
+++ WorkoutPlanner/Context/DatabaseContext.cs
@@ -6,13 +6,19 @@ namespace WorkoutPlanner.Context
 {
     public class DatabaseContext : IdentityDbContext<User>
     {
+        private IWebHostEnvironment _environment;
         public DbSet<Favourite> Favourites { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Workout> Workouts { get; set; }
         public DbSet<WorkoutLog> WorkoutsLogs { get; set; }
         public DbSet<PersonalTrainer> PersonalTrainers { get; set; }
 
-      
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options, IWebHostEnvironment environment) : base(options)
+        {
+            _environment = environment;
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionbuilder)
         {
             var folder = Environment.SpecialFolder.MyDocuments;
---
+++ WorkoutPlanner/Context/DatabaseSeeder.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WorkoutPlanner.Model;
+
+namespace WorkoutPlanner.Context
+{
+    public class DatabaseSeeder
+    {
+        private readonly DatabaseContext _context;
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public DatabaseSeeder(DatabaseContext context, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task Seed()
+        {
+            await _context.Database.MigrateAsync();
+
+            if (!_context.Users.Any())
+            {
+
+
+                await _roleManager.CreateAsync(new IdentityRole("PersonalTrainer"));
+                await _roleManager.CreateAsync(new IdentityRole("Client"));
+
+                 
+            }
+
+    }
